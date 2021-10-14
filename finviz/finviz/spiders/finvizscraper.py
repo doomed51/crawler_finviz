c5a4a1ffd2e83da8e54db8e6305ee484c50fc1ff
Rachit <racshankar@deloitte.ca>
2021-10-14 15:18:31 -0400

    updated to use pandas to read excel and populate the URLs to be crawled

--- finviz/finviz/spiders/finvizscraper.py
+++ finviz/finviz/spiders/finvizscraper.py
@@ -6,41 +6,50 @@
 ##### This code is built upon the tutorial here: https://www.babbling.fish/scraping-for-a-job/
 
 import scrapy
+import pandas as pd
 
 from bs4 import BeautifulSoup
 from scrapy.utils.python import retry_on_eintr
+from openpyxl import workbook, load_workbook
+from openpyxl.worksheet import worksheet
 #from itertools import product
 
-tickers = "IEP"
+#TODO make the filename relative...
+filePath = "F:/workbench/crawler_finviz/venv/Sample.xlsm"
+
+#load up the excel file 
+workbook = load_workbook(filename=filePath)
+df = pd.read_excel(filePath, "Valuations")
+#df.set_index('Symbol', inplace=True)
+
+#select sheet by name 
+#sheet = workbook["Valuations"]
+
+# set the URLs that need to be crawled
+# this is the unique set of Symbols from the excel doc 
 urls = []
+for item in df['Symbol'].unique():
+    urls.append(f"http://finviz.com/quote.ashx?t={item}")
 
-urls.append(f"http://finviz.com/quote.ashx?t={tickers}")
+# 
+#:::::::::::::::::::::::::::::::
+#
+# debug code :::::::::::::::::::
+#
+#:::::::::::::::::::::::::::::::
+#
+# 
+print("")
+print("-------------")
+print("#############")
+print("")
 
-class FinvizscraperSpider(scrapy.Spider):
-    name = 'finvizscraper'
-    allowed_domains = ['finviz.com']
-    start_urls = urls #['http://finviz.com/']
+#df.at['IEP', "Market Cap"] = 13.2
+#print(df.loc['IEP'])
+#print(df['Market Cap'])
+print(df['Symbol'].unique())
 
-    def parse(self, response):
-        soup = BeautifulSoup(response.text, features="lxml")
-
-        #find the stats table
-        tickerStats = soup.find("table", {"class":"snapshot-table2"})
-
-        #find all tabledata 
-        statsTable = soup.find_all("table",{"class":"snapshot-table2"})
-
-        marketCap = soup.find("td", text="Market Cap").find_next_sibling("td").text
-
-        print("#############")
-        print("#############")
-        print("")
-        
-        print(marketCap)
-        
-        print("")
-        print("#############")
-        print("#############")
-    
-        #for tickerStat in tickerStats:
-            #how does beautifulsoup organize/structure the tabledata 
+print("")
+print("#############")
+print("-------------")
+print("")
