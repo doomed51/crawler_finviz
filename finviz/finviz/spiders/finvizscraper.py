710920c5a755bae70c3338e0aeb9dc4f1f62b43b
Rachit <racshankar@deloitte.ca>
2021-10-19 11:28:57 -0400

    implemented write to excel afterscraping

--- finviz/finviz/spiders/finvizscraper.py
+++ finviz/finviz/spiders/finvizscraper.py
@@ -5,8 +5,10 @@
 
 ##### This code is built upon the tutorial here: https://www.babbling.fish/scraping-for-a-job/
 
+import openpyxl
 import scrapy
 import pandas as pd
+import openpyxl
 
 from scrapy.utils.python import retry_on_eintr
 from bs4 import BeautifulSoup
@@ -19,16 +21,16 @@ from scrapy.utils.log import configure_logging
 decimalConversion = {'M' : 6, 'B': 9}
 
 #TODO make the filename relative...
+# Load Valuations sheet in the excel file into a dataframe
 filePath = "F:/workbench/crawler_finviz/venv/Sample.xlsm"
+valuations_df = pd.read_excel(filePath, "Valuations") 
+#valuations_df = openpyxl.load_workbook(filePath, keep_vba=True)
 
-#load up the excel file 
-#workbook = load_workbook(filename=filePath)
-df = pd.read_excel(filePath, "Valuations")
 
 # set the URLs that need to be crawled
 # this is the unique set of Symbols from the excel doc 
 urls = []
-for item in df['Symbol'].unique():
+for item in valuations_df['Symbol'].unique():
     urls.append(f"http://finviz.com/quote.ashx?t={item}")
 
 # converts 'B' and 'M' suffixes into a number 
@@ -62,27 +64,47 @@ class FinvizscraperSpider(scrapy.Spider):
         # convert into number 
         marketCap = text_to_num(marketCap)
         
-        #populate the df with the marketcap data 
-        df.loc[df['Symbol'] == ticker,'Market Cap'] = marketCap
+        #populate the valuations_df with the marketcap data 
+        valuations_df.loc[valuations_df['Symbol'] == ticker,'Market Cap'] = marketCap
         
         # TODO POPULATE CURRENT PRICE 
         #::::::::::::::::::::::::::::
         currentPrice = soup.find("td", text="Price").find_next_sibling("td").text
-        df.loc[df['Symbol'] == ticker,'Current Price'] = currentPrice
+        valuations_df.loc[valuations_df['Symbol'] == ticker,'Current Price'] = currentPrice
         
         # TODO POPULATE PRICE TO BOOK 
         #::::::::::::::::::::::::::::
         priceToBook = soup.find("td", text="P/B").find_next_sibling("td").text
-        df.loc[df['Symbol'] == ticker,'Price-to-Book'] = priceToBook
+        valuations_df.loc[valuations_df['Symbol'] == ticker,'Price-to-Book'] = priceToBook
         
-        print(df)
+        #print(valuations_df)
         # TODO POPULATE COMPANY NAME 
         #::::::::::::::::::::::::::::
 
+        print("Scraped: " + ticker)
+
 # code to run the spider as a script 
+print("Starting to scrape!!")
+print("")
 runner = CrawlerRunner()
 
 d = runner.crawl(FinvizscraperSpider)
 d.addBoth(lambda _: reactor.stop())
 
-reactor.run()
+reactor.run() #script will block here until the crawling is finished 
+print("")
+print ("Scraping complete!!")
+print("")
+
+# TODO fix index printing and symbol column name issue
+myBook = openpyxl.load_workbook(filePath, keep_vba=True)
+
+with pd.ExcelWriter(filePath, engine='openpyxl') as writer: 
+    writer.book = myBook
+    writer.sheets = dict((ws.title, ws) for ws in myBook.worksheets)
+    writer.vba_archive = myBook.vba_archive
+
+    #valuations_df.to_excel(writer, sheet_name='Valuations', header=True, index=True, startrow=1, startcol=0, columns=['A'])
+    valuations_df.to_excel(writer, sheet_name='Valuations', header=True, index=False)
+
+    writer.save()
